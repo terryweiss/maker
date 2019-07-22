560720f5e2c8ba84fa6b9154a307b89bc50d1bec
Terry Weiss <terry.weiss@concorde2000.com>
2019-07-22 16:36:11 -0400

    Creating version 1.9.9

--- src/npm.mk
+++ src/npm.mk
@@ -5,4 +5,4 @@ Description.NPM-MK:=##@npm-mk Tools for working with the npm repository
 publish-npm: ${README-FILE-OUT} ${PACKAGEJSON-OUT}##@npm-mk This will call npm publish
 	$(call LoadCurrentVersion)
 	$(call show-msg,Publishing ${CURRENT-VERSION})
-	@cd ${OUT-DIR} && ${NPM} publish --access=public
+	@cd ${OUT-DIR} && ${NPM} publish --access=public --non-interactive
